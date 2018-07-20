cfe32956791663afe56923925a9b3605c8ea2d14
Sean Willison <sean.willison@bcbsfl.com>
2018-07-20 09:05:14 -0400

    setting up static files location for server

--- ui-new/server/routes/static.js
+++ ui-new/server/routes/static.js
@@ -1,7 +1,15 @@
+/* eslint-disable class-methods-use-this */
+const express = require('express');
+
 class StaticRoutes {
-    init(app){
-        app.get('/', (req, res) => res.send('Hello STATIC!'));
-    }
+  init(app) {
+    const staticFilesPath = `${__dirname}/../public`;
+    console.debug('Static Files Firectory', staticFilesPath);
+    const location = express.static(staticFilesPath);
+
+    app.use('/', location);
+    app.use('/workflow/*', location);
+  }
 }
 
 module.exports = StaticRoutes;
