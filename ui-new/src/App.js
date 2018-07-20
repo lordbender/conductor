789982f85b142e3fd5b49bd8fc62e110f523f873
Sean Willison <sean.willison@bcbsfl.com>
2018-07-20 16:50:45 -0400

    Adding Styled Components and AppBar

--- ui-new/src/App.js
+++ ui-new/src/App.js
@@ -1,15 +1,26 @@
 import React from 'react';
+import styled from 'styled-components';
 import { BrowserRouter } from 'react-router-dom';
 import HelpRoutes from 'views/Help';
 import WorkflowRoutes from 'views/Workflow';
+import AppBar from 'components/AppBar';
+
+const MainContentArea = styled.div`
+  padding-left: 50px;
+  padding-right: 50px;
+  padding-top: 20px;
+`;
 
 const App = () => (
-  <BrowserRouter>
-    <div>
-      <HelpRoutes />
-      <WorkflowRoutes />
-    </div>
-  </BrowserRouter>
+  <div>
+    <AppBar />
+    <BrowserRouter>
+      <MainContentArea>
+        <HelpRoutes />
+        <WorkflowRoutes />
+      </MainContentArea>
+    </BrowserRouter>
+  </div>
 );
 
 export default App;
