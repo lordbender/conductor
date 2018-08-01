0589efe32695950aa8885bc21fe4cd88b81f108a
Sean Willison <sean.willison@gmail.com>
2018-08-01 17:09:26 -0400

    SideNav rendering - updates

---
+++ ui/src/stores/metadata.js
@@ -0,0 +1,27 @@
+import { createAction, handleActions } from 'redux-actions';
+import axios from 'axios';
+
+const defaultState = {
+  list: [],
+  detail: {}
+};
+
+const prefix = 'METADATA';
+export const setMetadataList = createAction(`${prefix}/SET_METADATA_LIST`);
+export const setMetadataDetail = createAction(`${prefix}/SET_METADATA_DETAIL`);
+
+// https://redux-actions.js.org/introduction/tutorial
+export default handleActions(
+  {
+    [setMetadataList]: (state, payload) => {
+      return { ...state, list: [].concat(payload || []) };
+    }
+  },
+  defaultState
+);
+
+export const listWorkflowDefs = () => dispatch => {
+  return axios.get('/api/wfe/metadata/workflow').then(({ data }) => {
+    dispatch(setMetadataList(data));
+  });
+};
