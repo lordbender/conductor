82951af0974cade3a75fdd70598959146024b743
Sean Willison <sean.willison@gmail.com>
2018-08-07 14:15:32 -0400

    hard rename

---
+++ ui/src/_pp.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import styled from 'styled-components';
+import HomeRoutes from 'views/Home';
+import MetadataRoutes from 'views/Metadata';
+import WorkflowRoutes from 'views/Workflow';
+// import Footer from './components/Footer';
+import LeftMenu from 'components/LeftMenu';
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
+const Column = styled.div`
+  width: ${props => props.width || '100%'};
+  flex-direction: row;
+  padding: ${props => props.padding || 0}px;
+`;
+
+const App = () => (
+  <Container>
+    <Column width="20%" padding={5}>
+      <LeftMenu version="1.0.0" />
+    </Column>
+    <Column width="80%" padding={15}>
+      <HomeRoutes />
+      <MetadataRoutes />
+      <WorkflowRoutes />
+    </Column>
+    {/* <Footer /> */}
+  </Container>
+);
+
+export default App;
