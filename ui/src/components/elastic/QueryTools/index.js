15da40a93f13eb470a86b460dea0a384e02c7e47
Sean Willison <sean.willison@bcbsfl.com>
2018-08-15 10:53:16 -0400

    framing up routes on React Side

--- ui/src/components/elastic/QueryTools/index.js
+++ ui/src/components/elastic/QueryTools/index.js
@@ -1,28 +1,68 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Button } from 'react-bootstrap';
+
+const Wrapper = styled.div`
+  padding: 15px;
+  display: flex;
+  flex-direction: row;
+`;
 
 const FormWrapper = styled.div`
+  padding: 15px;
+  display: flex;
+  flex-direction: column-reverse;
+`;
+
+const Form = styled.div`
+  padding: 15px;
+`;
+
+const Actions = styled.div`
+  padding: 15px;
+`;
+
+const QueryResult = styled.div`
   padding: 15px;
 `;
 
 class QueryTools extends React.Component {
   state = {
-    query: ''
+    query: '',
+    result: 'Result Placeholder...'
+  };
+
+  executeQuery = () => {
+    const { query, result } = this.state;
+
+    console.log('query => ', query);
+    this.setState({ result: query });
   };
 
   render() {
-    const { query } = this.state;
+    const { query, result } = this.state;
+
     return (
-      <FormWrapper>
-        <textarea
-          value={query}
-          onChange={({ target: { value: query = '' } }) => {
-            this.setState({ query });
-          }}
-          cols="120"
-          rows="30"
-        />
-      </FormWrapper>
+      <Wrapper>
+        <FormWrapper>
+          <Form>
+            <textarea
+              value={query}
+              onChange={({ target: { value: query = '' } }) => {
+                this.setState({ query });
+              }}
+              cols="120"
+              rows="30"
+            />
+            <Actions>
+              <Button bsStyle="danger" onClick={this.executeQuery}>
+                Execute
+              </Button>
+            </Actions>
+          </Form>
+        </FormWrapper>
+        <QueryResult>{result}</QueryResult>
+      </Wrapper>
     );
   }
 }
