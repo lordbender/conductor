780ff2217f09e7c1406f404a7c0118f73132a117
Sean Willison <sean.willison@gmail.com>
2018-08-27 19:43:10 -0400

    Moving to a responsive layout

--- ui/src/components/TopNav/index.js
+++ ui/src/components/TopNav/index.js
@@ -13,14 +13,19 @@ const TopNav = props => (
       </Navbar.Brand>
     </Navbar.Header>
     <Nav>
-      <NavItem eventKey={1} href="#" onClick={() => props.history.push('/workflow')}>
-        Workflows
+      <NavItem eventKey={1} onClick={() => props.history.push('/events')}>
+        <i className="fa fa-circle-thin" />
+        &nbsp; Event Handlers
       </NavItem>
-      <NavItem eventKey={2} href="#" onClick={() => props.history.push('/workflow/metadata')}>
-        Workflow Defs
+      <NavItem eventKey={2} onClick={() => props.history.push('/workflow/metadata')}>
+        <i className="fa fa-star" />
+        &nbsp; Workflow Defs
       </NavItem>
-      <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
-        <MenuItem eventKey={3.1}>Action</MenuItem>
+      <NavDropdown eventKey={3} title="Workflow" id="basic-nav-dropdown">
+        <MenuItem eventKey={3.1} onClick={() => props.history.push('/workflow')}>
+          <i className="fa fa-circle-thin" />
+          &nbsp; All
+        </MenuItem>
         <MenuItem eventKey={3.2}>Another action</MenuItem>
         <MenuItem eventKey={3.3}>Something else here</MenuItem>
         <MenuItem divider />
