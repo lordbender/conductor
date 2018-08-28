24216506e5840369430fc10c7155c224766f2891
Sean Willison <sean.willison@gmail.com>
2018-08-28 13:53:35 -0400

    setting up to move to independant redux stores for each section

--- ui/src/components/workflow/executions/WorkflowList.js
+++ ui/src/components/workflow/executions/WorkflowList.js
@@ -79,12 +79,12 @@ class WorkflowList extends React.Component {
   constructor(props) {
     super(props);
     const {
-      location: { query: { workflowTypes = '', q = '', status = '', start = 0, h } = {} }
+      location: { query: { q = '', status = '', start = 0, h } = {} }
     } = props;
 
     this.state = {
       search: q === 'undefined' || q === '' ? '' : q,
-      workflowTypes: workflowTypes === '' ? [] : workflowTypes.split(','),
+
       status: status !== '' ? status.split(',') : [],
       h,
       update: true,
@@ -97,66 +97,6 @@ class WorkflowList extends React.Component {
     await this.props.listWorkflows();
   }
 
-  searchBtnClick = () => {
-    this.state.update = true;
-    this.refreshResults();
-  };
-
-  refreshResults = () => {
-    if (this.state.update) {
-      this.state.update = false;
-      this.urlUpdate();
-      this.doDispatch();
-    }
-  };
-
-  workflowTypeChange = workflowTypes => {
-    this.state.update = true;
-    this.state.workflowTypes = workflowTypes;
-    this.refreshResults();
-  };
-
-  statusChange = status => {
-    this.state.update = true;
-    this.state.status = status;
-    this.refreshResults();
-  };
-
-  nextPage = () => {
-    this.state.start = 100 + parseInt(this.state.start, 10);
-    this.state.update = true;
-    this.refreshResults();
-  };
-
-  prevPage = () => {
-    this.state.start = parseInt(this.state.start, 10) - 100;
-    if (this.state.start < 0) {
-      this.state.start = 0;
-    }
-    this.state.update = true;
-    this.refreshResults();
-  };
-
-  searchChange = e => {
-    const val = e.target.value;
-    this.setState({ search: val });
-  };
-
-  hourChange = e => {
-    this.state.update = true;
-    this.state.h = e.target.value;
-    this.refreshResults();
-  };
-
-  keyPress = e => {
-    if (e.key == 'Enter') {
-      this.state.update = true;
-      const q = e.target.value;
-      this.setState({ search: q });
-      this.refreshResults();
-    }
-  };
-
   prefChange = e => {
     this.setState({
       fullstr: e.target.checked
@@ -166,122 +106,12 @@ class WorkflowList extends React.Component {
   };
 
   render() {
-    // const {totalHits, hits} = this.props;
-    let wfs = [];
-    let filteredWfs = [];
-
-    let totalHits = 0;
-    let found = 0;
-    if (this.props.hits) {
-      wfs = this.props.hits;
-      totalHits = this.props.totalHits;
-      found = wfs.length;
-    }
-    const start = parseInt(this.state.start, 10);
-    let max = start + 100;
-    if (found < 100) {
-      max = start + found;
-    }
-    // const workflowNames = this.state.workflows ? this.state.workflows : [];
-    // const statusList = ['RUNNING', 'COMPLETED', 'FAILED', 'TIMED_OUT', 'TERMINATED', 'PAUSED'];
-
-    // secondary filter to match sure we only show workflows that match the the status
-    const currentStatusArray = this.state.status;
-    if (currentStatusArray.length > 0 && wfs.length > 0) {
-      filteredWfs = wfs.filter(wf => currentStatusArray.includes(wf.status));
-    } else {
-      filteredWfs = wfs;
-    }
+    const { totalHits = 0, hits = [] } = this.props;
 
     return (
       <div className="ui-content">
-        <div>
-          <Panel header="Filter Workflows (Press Enter to search)">
-            <Grid fluid>
-              <Row className="show-grid">
-                <Col md={4}>
-                  <FormControl
-                    type="text"
-                    placeholder="Search"
-                    value={this.state.search}
-                    onKeyPress={this.keyPress}
-                    onChange={this.searchChange}
-                  />
-                  &nbsp;
-                  <i className="fa fa-angle-up fa-1x" />
-                  &nbsp;&nbsp;
-                  <label className="small nobold">Free Text Query</label>
-                  &nbsp;&nbsp;
-                  <input type="checkbox" checked={this.state.fullstr} onChange={this.prefChange} />
-                  <label className="small nobold">&nbsp;Search for entire string</label>
-                </Col>
-                <Col md={4}>
-                  {/* <Typeahead
-                    onChange={this.workflowTypeChange}
-                    options={workflowNames}
-                    placeholder="Filter by workflow type"
-                    multiple
-                    selected={this.state.workflowTypes}
-                  /> */}
-                  &nbsp;
-                  <i className="fa fa-angle-up fa-1x" />
-                  &nbsp;&nbsp;
-                  <label className="small nobold">Filter by Workflow Type</label>
-                </Col>
-                <Col md={2}>
-                  {/* <Typeahead
-                    onChange={this.statusChange}
-                    options={statusList}
-                    placeholder="Filter by status"
-                    selected={this.state.status}
-                    multiple
-                  /> */}
-                  &nbsp;
-                  <i className="fa fa-angle-up fa-1x" />
-                  &nbsp;&nbsp;
-                  <label className="small nobold">Filter by Workflow Status</label>
-                </Col>
-                <Col md={2}>
-                  <FormControl className="number-input" type="text" value={this.state.h} onChange={this.hourChange} />
-                  &nbsp;&nbsp;&nbsp;
-                  <Button bsStyle="success" onClick={this.searchBtnClick} className="search-label btn">
-                    <i className="fa fa-search" />
-                    &nbsp;&nbsp;Search
-                  </Button>
-                  <br />
-                  &nbsp;&nbsp;&nbsp;
-                  <i className="fa fa-angle-up fa-1x" />
-                  &nbsp;&nbsp;
-                  <label className="small nobold">Created (in past hours)</label>
-                </Col>
-              </Row>
-            </Grid>
-            <form />
-          </Panel>
-        </div>
-        <span>
-          Total Workflows Found: <b>{totalHits}</b>, Displaying {this.state.start} <b>to</b> {max}
-        </span>
-        <span style={{ float: 'right' }}>
-          {parseInt(this.state.start, 10) >= 100 ? (
-            <a onClick={this.prevPage}>
-              <i className="fa fa-backward" />
-              &nbsp;Previous Page
-            </a>
-          ) : (
-            ''
-          )}
-          {parseInt(this.state.start, 10) + 100 <= totalHits ? (
-            <a onClick={this.nextPage}>
-              &nbsp;&nbsp;Next Page&nbsp;
-              <i className="fa fa-forward" />
-            </a>
-          ) : (
-            ''
-          )}
-        </span>
         <BootstrapTable
-          data={filteredWfs}
+          data={hits}
           striped
           hover
           search={false}
@@ -320,9 +150,6 @@ class WorkflowList extends React.Component {
             &nbsp;
           </TableHeaderColumn>
         </BootstrapTable>
-
-        <br />
-        <br />
       </div>
     );
   }
